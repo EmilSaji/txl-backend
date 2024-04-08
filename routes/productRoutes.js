e3c7fc359ea3bb595f55f7b868ac53372f30ee68
Emil Saji <emilsaji123@gmail.com>
2024-04-09 01:32:04 +0530

    Backend Setup

---
+++ routes/productRoutes.js
@@ -0,0 +1,74 @@
+const express = require("express");
+
+module.exports = () => {
+  const router = express.Router();
+
+  // fetch all products
+  router.get("/products", async (req, res) => {
+    try {
+      const connection = req.app.locals.connection;
+      const [rows, fields] = await connection.execute("SELECT * FROM products");
+      res.json(rows);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // fetch product by ID
+  router.get("/products/:id", async (req, res) => {
+    try {
+      const connection = req.app.locals.connection;
+      const [rows, fields] = await connection.execute(
+        "SELECT * FROM products WHERE id = ?",
+        [req.params.id]
+      );
+      res.json(rows[0]);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // create new product
+  router.post("/create-product", async (req, res) => {
+    try {
+      const connection = req.app.locals.connection;
+      const { name, price } = req.body;
+      const [result] = await connection.execute(
+        "INSERT INTO products (name, price) VALUES (?, ?)",
+        [name, price]
+      );
+      res.json({ id: result.insertId, name, price });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // update product
+  router.put("/update-products/:id", async (req, res) => {
+    try {
+      const connection = req.app.locals.connection;
+      const { name, price } = req.body;
+      await connection.execute(
+        "UPDATE products SET name = ?, price = ? WHERE id = ?",
+        [name, price, req.params.id]
+      );
+      res.json({ id: req.params.id, name, price });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // delete product
+  router.delete("/delete-products/:id", async (req, res) => {
+    try {
+      const connection = req.app.locals.connection;
+      await connection.execute("DELETE FROM products WHERE id = ?", [
+        req.params.id,
+      ]);
+      res.json({ message: "Product deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  return router;
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mysql = require("mysql2/promise");
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+app.use(express.json());
+app.use("/", productRoutes());
+
+mysql
+  .createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true,
+      ...(process.env.CA_CERT_PATH && {
+        ca: require("fs").readFileSync(process.env.CA_CERT_PATH),
+      }),
+    },
+  })
+  .then((connection) => {
+    console.log("Successfully connected to MySQL database!");
+    app.locals.connection = connection;
+    productRoutes(app);
+
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    });
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to connect to the database: ${error.message}`);
+    process.exit(1);
+  });
